5db3bf42dc6fc4bb80698875a3069f92d9346b3a
cleancoderocker <scalarocker@googlemail.com>
2016-12-09 10:39:14 +0100

    Updated test

--- _sensors-project/dummy-sensor/test/DummySensorTest.js
+++ _sensors-project/dummy-sensor/test/DummySensorTest.js
@@ -10,39 +10,4 @@ describe('DummySensor', () =>
         let dummySensor = new DummySensor();
         assert.equal(dummySensor instanceof DummySensor, true);
     });
-    it('should start and stop', done =>
-    {
-        let dummySensor = new DummySensor();
-        dummySensor.start().then(() => {
-            dummySensor.stop().then(() => {
-                done();
-            });
-        });
-    });
-    it('should generate value', done =>
-    {
-        let dummySensor = new DummySensor();
-        dummySensor.start().then(() => {
-            setTimeout(() => {
-                dummySensor.stop().then(() => {
-                    done();
-                });
-            }, 500);
-        });
-    });
-    it('should generate value', done =>
-    {
-        let dummySensor = new DummySensor();
-        dummySensor.onchange = event => {
-            assert.equal(event.reading.dummyValue > 0, true);
-            assert.equal(event.reading.dummyValue < 1, true);
-        }
-        dummySensor.start().then(() => {
-            setTimeout(() => {
-                dummySensor.stop().then(() => {
-                    done();
-                });
-            }, 500);
-        });
-    });
 });
